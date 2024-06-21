96b0f7d6ded1d85d5c3170a306bd53a4e1f319e1
Yash Sharma <yashkarma7410@gmail.com>
2024-06-21 13:26:53 +0530

    sorting0and1

--- Day3_1_VCTR.cpp
+++ Day3_1_VCTR.cpp
@@ -4,17 +4,39 @@
 
 using namespace std;
 
- 
+
+void sort0sand1s (int a[] , int size){
+    int i = 0;
+    int j = size-1;
+    while(i<j){
+        if( (a[i] == 1) &&(a[j] == 0) ){
+            swap(a[i],a[j]);
+            i++;
+            j--;
+        }else if(a[i]==0){
+            i++;
+        }else if(a[j]==1){
+            j--;
+        }
+    }
+     
+};
+
+
+void display(int a[], int size){
+    cout << "[ " ;
+    for(int i = 0 ; i<size ; i++){
+        cout << a[i] << " ";
+    }
+    cout << "]" ;
+    cout << endl;
+}
 
 
 int main(){
-    vector<int> x = {1,2,3,4};
-    vector<int>::iterator i;
+   int x[] = {1,0,1,1,1,0,0,1,0,1,0};
+   int s = sizeof(x)/sizeof(x[0]);
 
-    i = x.begin();
-
-
-
-    cout << *i << endl;
-
+   sort0sand1s(x,s);
+   display(x,s);
 }
